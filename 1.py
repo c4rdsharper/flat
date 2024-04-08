acf478d82055a508f7a2a8a44dcadbec245d10b9
Andrey <nyuvash@mail.ru>
2024-04-08 03:33:01 +0300

    1

--- 1.py
+++ 1.py
@@ -14,7 +14,29 @@ import matplotlib.ticker as ticker
 st.title('АНАЛИЗ РЫНКА НЕДВИЖИМОСТИ Г.УЛАН-УДЭ')
 
 #Считываем Датафрейм
+df = pd.read_csv('cian')
+del df["Unnamed: 0.1"]
+del df["Unnamed: 0"]
+
 con_pg = create_engine('postgresql+psycopg2://mycliaow:ntV1e4tt7w3EEIW1I-VS-vnzUTGmxezq@abul.db.elephantsql.com/mycliaow')
+def psql_insert_copy(table, conn, keys, data_iter):
+    # gets a DBAPI connection that can provide a cursor
+    dbapi_conn = conn.connection
+    with dbapi_conn.cursor() as cur:
+        s_buf = StringIO()
+        writer = csv.writer(s_buf)
+        writer.writerows(data_iter)
+        s_buf.seek(0)
+        columns = ', '.join('"{}"'.format(k) for k in keys)
+        if table.schema:
+            table_name = '{}.{}'.format(table.schema, table.name)
+        else:
+            table_name = table.name
+        sql = 'COPY {} ({}) FROM STDIN WITH CSV'.format(
+            table_name, columns)
+        cur.copy_expert(sql=sql, file=s_buf)
+
+df.to_sql('cian',con_pg,index=False,if_exists='replace',method=psql_insert_copy)
 def select(sql):
     with con_pg.connect() as conn:
         return pd.read_sql(sql_text(sql), conn)
@@ -33,7 +55,7 @@ def distreverse(n):
         return 2
     else:
         return 3
-df = select('''select * from cian''')
+
 #Слайдер
 values = st.slider(
     'Выберите диапазон цен',
